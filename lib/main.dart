e637d6aaf98ee406ae2217b40edc7fab1286f92f
ujjwal <ujjwalmaity98@gmail.com>
2020-05-03 13:10:19 +0530

    Layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -11,44 +11,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            //mainAxisAlignment: MainAxisAlignment.end,
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //crossAxisAlignment: CrossAxisAlignment.end,
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            //verticalDirection: VerticalDirection.up,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
                 width: 100.0,
-                height: 100.0,
-                //margin: EdgeInsets.all(20.0),
-                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-                //margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-                //margin: EdgeInsets.only(left: 30.0),
-                //padding: EdgeInsets.all(20.0),
-                color: Colors.white,
-                child: Text("Container 1"),
+                color: Colors.red,
               ),
-              SizedBox(
-                height: 20.0,
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.lightGreen,
+                    ),
+                  ],
+                ),
               ),
               Container(
                 width: 100.0,
-                height: 100.0,
                 color: Colors.blue,
-                child: Text("Container 2"),
               ),
-              Container(
-                height: 100.0,
-                color: Colors.red,
-                child: Text("Container 3"),
-              ),
-              /*Container(
-                width: double.infinity,
-                height: 10.0,
-              ),*/
             ],
           ),
         ),
