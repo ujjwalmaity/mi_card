c0dbc3975860c99905d32324bdf220c987df6917
ujjwal <ujjwalmaity98@gmail.com>
2020-05-03 12:43:08 +0530

    Row & column Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,45 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            width: 100.0,
-            height: 100.0,
-            //margin: EdgeInsets.all(20.0),
-            //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            //margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text("Hello"),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            //mainAxisAlignment: MainAxisAlignment.end,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //verticalDirection: VerticalDirection.up,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: 100.0,
+                //margin: EdgeInsets.all(20.0),
+                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+                //margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+                //margin: EdgeInsets.only(left: 30.0),
+                //padding: EdgeInsets.all(20.0),
+                color: Colors.white,
+                child: Text("Container 1"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text("Container 2"),
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: Text("Container 3"),
+              ),
+              /*Container(
+                width: double.infinity,
+                height: 10.0,
+              ),*/
+            ],
           ),
         ),
       ),
